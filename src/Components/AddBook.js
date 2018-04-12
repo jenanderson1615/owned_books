bb5423153f8c6c65a15121f924779a8fe3444f25
Jennifer Anderson <jennifer@macpractice.com>
2018-04-12 14:18:57 -0500

    Add title bar and buttons to the child pages

--- src/Components/AddBook.js
+++ src/Components/AddBook.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import AppBar from 'material-ui/AppBar' 
+import RaisedButton from 'material-ui/RaisedButton'
+import Link from 'react-router-dom/Link'
+
+const titleStyle = {
+    textAlign: "center",
+};
 
 class AddBook extends React.Component {    
     render() {
         return (
             <div>
+                <AppBar
+                    title="Add a Book"
+                    titleStyle={titleStyle}
+                    showMenuIconButton={false}
+                />
                 <form>
                     <div> Title </div>
                     <div> Author </div>
                     <div> Description </div>
+                    <Link to={'/'}>
+                        <RaisedButton> Back </RaisedButton>
+                    </Link>
+                    <Link to={'/'}>
+                        <RaisedButton> Add </RaisedButton>
+                    </Link>
                 </form>
             </div>
         );
--- src/Components/BookDetail.js
+++ src/Components/BookDetail.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import AppBar from 'material-ui/AppBar'
+import Link from 'react-router-dom/Link'
+import RaisedButton from 'material-ui/RaisedButton'
 
-class BookDetail extends React.Component {    
+const titleStyle = {
+    textAlign: "center",
+};
+
+class BookDetail extends React.Component {
     render() {
         return (
             <div>
+                <AppBar
+                    title="Book Details"
+                    titleStyle={titleStyle}
+                    showMenuIconButton={false}
+                />
                 <div>{this.props.title}</div>
                 <div>{this.props.author}</div>
                 <div>{this.props.description}</div>
+                <Link to={'/'}>
+                    <RaisedButton> Back </RaisedButton>
+                </Link>
+                <Link to={'/'}>
+                    <RaisedButton> Delete </RaisedButton>
+                </Link>
             </div>
         );
     }
